67152e7341638fd6bc8ddc01248069793edac4bc
Adam <adam.alani@epita.fr>
2020-12-08 15:42:58 +0100

    Update handeld_halt.go

--- day 8/handeld_halt.go
+++ day 8/handeld_halt.go
@@ -1,21 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strings"
+	"strconv"
 )
 
 func main() {
 	data , _ := ioutil.ReadFile("input.txt")
 	step := strings.Split(strings.TrimSpace(string(data)), "\n")
+	halt(step)
 	
 
 }
 
 
 func halt(step []string) (int) {
-	
+	var acc int
+	var i int
+	s := make(map[string]bool)
+	for true {
+		if s[string(i)]{
+			return acc
+		}
+		s[string(i)] = true
+		line := step[i]
+		line = strings.Split(line , "")
+		val := line[1]
+		inst := line[0]
+		val , _ = strconv.Atoi(val)
+
+		println(step)
+
+
+		i += 1
+
+	}
+
+	return acc
 }
 
 
