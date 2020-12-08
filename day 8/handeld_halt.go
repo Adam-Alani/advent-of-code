c073ce30b73aed03a23218188f75c434cf4ffa52
Adam <adam.alani@epita.fr>
2020-12-08 14:55:39 +0100

    day 8

---
+++ day 8/handeld_halt.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	data , _ := ioutil.ReadFile("input.txt")
+	step := strings.Split(strings.TrimSpace(string(data)), "\n")
+	
+
+}
+
+
+func halt(step []string) (int) {
+	
+}
+
+
---
+++ day 8/handeld_halt.py
@@ -0,0 +1,61 @@
+import re
+data = open('input.txt').read().split('\n')
+data = list(data)
+
+
+def halt(data):
+    acc = 0
+    i = 0
+    passed = set()
+    while True:
+        if i in passed:
+            return(acc)
+        passed.add(i) 
+        step, val = data[i].split()
+        val = int(val)
+            
+        if step == "jmp":
+            i += val
+        elif step == "acc":
+            acc += val
+            i += 1
+        elif step == "nop":
+            i += 1
+
+
+def secondHalt(data):
+    acc = 0
+    i = 0
+    passed = set()
+    while True:
+        if i == len(data):
+            return(acc)
+        if i in passed:
+            return None
+
+        passed.add(i) 
+        step, val = data[i].split()
+        val = int(val)
+            
+        if step == "jmp":
+            i += val
+        elif step == "acc":
+            acc += val
+            i += 1
+        elif step == "nop":
+            i += 1
+
+def backTrack(data):
+    for i in range(len(data)):
+        copy = data[:]
+        if copy[i][:3] == "nop":
+            copy[i] = copy[i].replace("nop" , "jmp")
+        elif copy[i][:3] == "jmp":
+            copy[i] = copy[i].replace("jmp" , "nop" )
+        res = secondHalt(copy)
+        if res:
+            return(res)
+
+
+        
+print(backTrack(data))
