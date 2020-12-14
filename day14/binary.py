e6064065bdf0a7cbbb908462fc8549016e7835fa
Adam <adam.alani@epita.fr>
2020-12-14 17:34:24 +0100

    Update binary.py

--- day14/binary.py
+++ day14/binary.py
@@ -1,4 +1,3 @@
-from itertools import product
 def parse():
     data = [(pre.strip(), post.strip()) for pre, post in [line.strip().split("=") for line in open("input.txt").readlines()]]
     mem = {}
@@ -10,9 +9,10 @@ def parse():
             mem[key] = int(apply(mask , valint) , base=2)
     print(sum(mem.values()))
 def apply(mask , val):
-    pos = [(index, val) for index, val in enumerate(mask) if val != "X"]
-    for coord ,val2 in pos:
-        val = val[:coord] + val2 + val[coord+1:]
+    pos = []
+    for i in range(len(mask)):
+        if mask[i] != "X":
+            pos.append((i , mask[i]))
     for i , val2 in pos:
         val = val[:i] + val2 + val[i+1:]
     return val
