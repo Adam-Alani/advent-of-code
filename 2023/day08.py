11e70731d7d217f4410a0c21a719c69798a269eb
Adam-Alani <adamalany@gmail.com>
2023-12-08 11:11:06 +0100

    refacto

--- 2023/day08.py
+++ 2023/day08.py
@@ -1,36 +1,40 @@
 from math import lcm
+
 with open('day08') as f:
     data = f.read().splitlines()
 instructions = data[0].strip()
-dict = {}
+dirs = {}
 for line in data[2:]:
-    line = line.split('=')
-    line[0] = line[0].strip()
-    (left, right) = line[1].split(',')
-    left = left[2:]
-    right = right[1:-1]
-    dict[line[0]] = (left, right)
+    source, dest = line.split('=')
+    (left, right) = dest.split(',')
+    dirs[source.strip()] = (left[2:], right[1:-1])
+
 
 def find():
-    current,current_idx,steps  = "AAA", 0, 0
+    current, current_idx, steps = "AAA", 0, 0
     while current != "ZZZ":
-        if instructions[current_idx] == 'L': current = dict[current][0]
-        else: current = dict[current][1]
+        if instructions[current_idx] == 'L':
+            current = dirs[current][0]
+        else:
+            current = dirs[current][1]
         steps += 1
         current_idx = (1 + current_idx) % len(instructions)
     return steps
 
+
 def find2():
     current = []
-    for key in dict:
+    for key in dirs:
         if key[-1] == 'A':
             current.append(key)
     totalSteps = []
     for node in current:
-        steps,current_idx = 0,0
+        steps, current_idx = 0, 0
         while node[-1] != 'Z':
-            if instructions[current_idx] == 'L': node = dict[node][0]
-            else: node = dict[node][1]
+            if instructions[current_idx] == 'L':
+                node = dirs[node][0]
+            else:
+                node = dirs[node][1]
             steps += 1
             current_idx = (1 + current_idx) % len(instructions)
         totalSteps.append(steps)
@@ -38,4 +42,6 @@ def find2():
     for s in totalSteps[1:]:
         res = lcm(res, s)
     return res
+
+
 print(find2())
