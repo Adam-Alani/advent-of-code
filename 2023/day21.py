1a7766a602492f43a4f4ae3cf57e3dd37b380e9b
Adam-Alani <adamalany@gmail.com>
2023-12-21 16:53:44 +0100

    part 2 now is a thing

--- 2023/day21.py
+++ 2023/day21.py
@@ -11,13 +11,10 @@ for i, line in enumerate(data):
         if c == 'S':
             start = (j,i)
             grid[i][j] = '.'
-
+            break
 
 grid_size = len(grid) # 131
-grid_half = grid_size // 2 # 65
-max_steps = grid_size * 2
-
-
+grid_half,max_steps = grid_size // 2, grid_size * 2
 def count_paths(grid,x,y,steps):
     dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     q = set()
@@ -32,12 +29,16 @@ def count_paths(grid,x,y,steps):
                     nq.add((nx,ny))
         q = nq
         steps -= 1
-        if (steps-1) in [grid_half, grid_half+grid_size, grid_half+grid_size*2]:
-            print(steps-1, len(q))
-        if steps == 0:
-            return q
-    print(steps, len(q))
+        if (steps-1) in [grid_half, grid_half+grid_size, grid_half+max_steps]:
+            yield (max_steps - steps + grid_size, len(q))
+        if steps == grid_half:
+            break
 
-
-
-count_paths(grid, start[0], start[1], grid_size*3)
+vals = [var for var in count_paths(grid, start[0], start[1], max_steps + grid_size)]
+x2,y2 = vals[0]
+x1,y1 = vals[1]
+x0,y0 = vals[2]
+p1,p2 = (y1-y0)/(x1-x0),(y2-y1)/(x2-x1)
+a,b,c = (p2-p1)/(x2-x0), p1, y0
+x = 26501365
+print(a*(x-x1)*(x-x0)+b*(x-x0)+c)
